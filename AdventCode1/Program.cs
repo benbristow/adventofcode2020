06f763e4c6bc2da305678088111a5ad7a8e6184e
Ben Bristow <ben@bristow.me>
2020-12-02 19:07:08 +0000

    Day 1 & Day 2

---
+++ AdventCode1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace AdventCode1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = ReadFile();
+            var numbers = file.Split(Environment.NewLine).Select(int.Parse).ToArray();
+
+            Console.WriteLine(Part1(numbers));
+            Console.WriteLine(Part2(numbers));
+        }
+
+        private static int Part1(int[] numbers)
+        {
+            var combinations = (from item in numbers
+                from item2 in numbers
+                where item < item2
+                select new Tuple<int, int>(item, item2)).ToList();
+
+            var combination = combinations.Single(x => x.Item1 + x.Item2 == 2020);
+
+            return combination.Item1 * combination.Item2;
+        }
+
+        private static int Part2(int[] numbers)
+        {
+            var combinations = (
+                from item in numbers
+                from item2 in numbers
+                from item3 in numbers
+                where item < item2 && item2 < item3
+                select new Tuple<int, int, int>(item, item2, item3)).ToList();
+
+            var combination = combinations.Single(x => x.Item1 + x.Item2 + x.Item3 == 2020);
+
+            return combination.Item1 * combination.Item2 * combination.Item3;
+        }
+
+        private static string ReadFile()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = assembly.GetManifestResourceNames().Single(str => str.EndsWith("numbers.txt"));
+
+            using var stream = assembly.GetManifestResourceStream(resourceName);
+            using var reader = new StreamReader(stream);
+            
+            return reader.ReadToEnd();
+        }
+    }
+}
---
+++ AdventCode2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace AdventCode2
+{
+    class Program
+    {
+        private struct Puzzle
+        {
+            public int Selector1;
+            public int Selector2;
+            public char Character; 
+            public string Value;
+        }
+
+        static void Main(string[] args)
+        {
+            var passwords = ReadFile().Split(Environment.NewLine).Select(ParsePuzzle).ToList();
+
+            Console.WriteLine(passwords.Where(IsValidPasswordPart1).Count());
+            Console.WriteLine(passwords.Where(IsValidPasswordPart2).Count());
+        }
+
+        private static bool IsValidPasswordPart1(Puzzle puzzle)
+        {
+            var occurrences = puzzle.Value.ToCharArray().Count(x => x == puzzle.Character);
+
+            return occurrences >= puzzle.Selector1 && occurrences <= puzzle.Selector2;
+        }
+
+        private static bool IsValidPasswordPart2(Puzzle puzzle) =>
+            puzzle.Value[puzzle.Selector1 - 1] == puzzle.Character ^
+            puzzle.Value[puzzle.Selector2 - 1] == puzzle.Character;
+
+        private static Puzzle ParsePuzzle(string input)
+        {
+            var match = Regex.Match(input, @"(\d+)\-(\d+) ([a-z]): ([a-z]+)");
+
+            return new Puzzle()
+            {
+                Selector1 = int.Parse(match.Groups[1].Value),
+                Selector2 = int.Parse(match.Groups[2].Value),
+                Character = match.Groups[3].Value[0],
+                Value = match.Groups[4].Value
+            };
+        }
+
+        private static string ReadFile()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = assembly.GetManifestResourceNames().Single(str => str.EndsWith("input.txt"));
+
+            using var stream = assembly.GetManifestResourceStream(resourceName);
+            using var reader = new StreamReader(stream);
+
+            return reader.ReadToEnd();
+        }
+    }
+}
